136f682791bf1a5a53abc4b7556be08499ed4847
Federico Araya <federico.araya@gmail.com>
2022-11-16 00:39:35 -0300

    deslog agregado

--- js/LoginApp.js
+++ js/LoginApp.js
@@ -25,20 +25,32 @@ function passIncorrecto(){
 function loguear(){
   const logoLogin = document.querySelectorAll('.logoLogin')
   const saludo = document.querySelectorAll('.saludo')
+  const formRegistrarse = document.querySelector('.formRegistrarse')
+  const botonRegistrarse = document.querySelector('.botonRegistrarse') 
+  const botonLog = document.querySelector('.botonLog')
+  const deslog = document.querySelector('.deslog') 
+  const formLogin = document.querySelector('#formLogin')
     saludo[0].innerHTML = `Hola, ${usuarioIngresado}`    
     saludo[1].innerHTML = `Hola, ${usuarioIngresado}`  
     logoLogin[0].style.backgroundColor = "#66c70c"
     logoLogin[0].style.borderRadius = "40px"
     logoLogin[1].style.backgroundColor = "#66c70c"
     logoLogin[1].style.borderRadius = "40px"
+    formRegistrarse.classList = 'd-none'
+    botonRegistrarse.classList = 'd-none'
+    botonLog.classList = "btn btn-primary botonLog d-none"
+    deslog.classList = "btn btn-primary deslog"
+    formLogin.classList = 'd-none'
+    if (usuarios[indexUsuario].categoria == "admin" ){
+      const adminVisibleM = document.querySelector('.adminVisibleM')
+      const adminVisibleD = document.querySelector('.adminVisibleD')
+      adminVisibleD.classList = 'nav-link d-block adminVisibleD'
+      adminVisibleM.classList = 'nav-item d-block adminVisibleM'
+    }
+
 }
 
-if (usuarios[indexUsuario].categoria == "admin" ){
-  const adminVisibleM = document.querySelector('.adminVisibleM')
-  const adminVisibleD = document.querySelector('.adminVisibleD')
-  adminVisibleD.classList = 'nav-link d-block adminVisibleD'
-  adminVisibleM.classList = 'nav-item d-block adminVisibleM'
-}
+
 
 }    
 
