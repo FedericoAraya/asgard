e47028de29c0e3b9de7e66f6ee5cbd6e05228b80
Federico Araya <federico.araya@gmail.com>
2022-11-18 02:55:34 -0300

    productos se agregan al carrito

--- js/data.js
+++ js/data.js
@@ -1,6 +1,4 @@
-
 //DATA//
-const categorias = ["Bajo Asiento", "Sobre Cuadro", "Frame Bag","Manubrio"];
 
 const productos = [
   {
@@ -8,50 +6,50 @@ const productos = [
     categoria: "bajo asiento",
     precio: 3000,
     imagenProducto: "../images/bajo asiento 4l.jpg",
-    id:1,
+    id: 1,
   },
   {
     nombreProducto: "Bajo Asiento 14L",
     categoria: "bajo asiento",
     precio: 5500,
     imagenProducto: "../images/bajo asiento 14l.jpg",
-    id:2,
+    id: 2,
   },
   {
     nombreProducto: "Bolso Stem",
     categoria: "sobre cuadro",
     precio: 1500,
     imagenProducto: "../images/bolso stem.jpg",
-    id:3,
+    id: 3,
   },
-  
+
   {
     nombreProducto: "Medio Frame Bag",
     categoria: "frame bag",
     precio: 6500,
     imagenProducto: "../images/medio fram.jpg",
-    id:4,
+    id: 4,
   },
   {
     nombreProducto: "Medio Frame Doble Suspensión",
     categoria: "frame bag",
     precio: 6500,
     imagenProducto: "../images/Medio Fram dobleS.jpg",
-    id:5,
+    id: 5,
   },
   {
     nombreProducto: "Frame Completo",
     categoria: "frame bag",
     precio: 6500,
     imagenProducto: "../images/frame un bolsillo.jpg",
-    id:6,
+    id: 6,
   },
   {
     nombreProducto: "Frame Doble Bolsillo",
     categoria: "frame bag",
     precio: 6500,
     imagenProducto: "../images/frame bag doble bolsillo.jpg",
-    id:7,
+    id: 7,
   },
   {
     nombreProducto: "Bolso Botella ",
@@ -65,10 +63,9 @@ const productos = [
     categoria: "manubrio",
     precio: 6000,
     imagenProducto: "../images/alforja de manubrio.jpg",
-    id:9,
+    id: 9,
   },
 ];
-
 
 class Producto {
   constructor(nombreProducto, categoria, precio, imagenProducto, id) {
@@ -80,17 +77,14 @@ class Producto {
   }
 }
 
-
-
-
 //data//
 
-let usuarioLogueado = "deslog"
-let carrito 
-let usuarios =[]
+let usuarioLogueado = "deslog";
+let carrito;
+let usuarios = [];
 
-if (localStorage.getItem('usuarios')) {
-  usuarios = JSON.parse(localStorage.getItem('usuarios'))
+if (localStorage.getItem("usuarios")) {
+  usuarios = JSON.parse(localStorage.getItem("usuarios"));
 } else {
   usuarios = [
     {
@@ -107,37 +101,72 @@ if (localStorage.getItem('usuarios')) {
       estado: "ok",
       email: "fede@hola.com",
     },
-  ]
- 
+  ];
 }
 
-
-if (localStorage.getItem('carrito')) {
-  carrito = JSON.parse(localStorage.getItem('carrito'))
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"));
 } else {
-  carrito = []
-}
-if (localStorage.getItem('usuarioLogueado')) {
-  usuarioLogueado = JSON.parse(localStorage.getItem('usuarioLogueado'))
-} 
-
-if(usuarioLogueado != "deslog"){
-  const logoLogin = document.querySelectorAll('.logoLogin')
-  const saludo = document.querySelectorAll('.saludo')
-    saludo[0].innerHTML = `Hola, ${usuarios[usuarioLogueado].nombreUsuario}`    
-    saludo[1].innerHTML = `Hola, ${usuarios[usuarioLogueado].nombreUsuario}`  
-    logoLogin[0].style.backgroundColor = "#66c70c"
-    logoLogin[0].style.borderRadius = "40px"
-    logoLogin[1].style.backgroundColor = "#66c70c"
-    logoLogin[1].style.borderRadius = "40px"
-    if (usuarios[usuarioLogueado].categoria == "admin" ){
-      const adminVisibleM = document.querySelector('.adminVisibleM')
-      const adminVisibleD = document.querySelector('.adminVisibleD')
-      adminVisibleD.classList = 'nav-link d-block adminVisibleD'
-      adminVisibleM.classList = 'nav-item d-block adminVisibleM'
-    }
-    
+  carrito = [];
 }
 
+if (localStorage.getItem("usuarioLogueado")) {
+  usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+}
 
+const cardsCarrito = document.querySelector(".cardCarrito");
 
+if (carrito.length > 0) {
+  renderizarCarrito();
+}
+
+function renderizarCarrito() {
+  carrito.forEach((producto) => {
+    const cardCarrito = document.createElement("div");
+    cardCarrito.classList = "card mb-3";
+    cardCarrito.setAttribute("idProd", producto.id);
+    cardCarrito.style = "max-height: 100px;";
+    cardCarrito.innerHTML = `        
+        <div class="row g-0">
+        <div class="col-6">
+        <img
+        src="${producto.imagen}"
+        class="img-fluid rounded-start"
+        alt="..."
+        />
+        </div>
+        <div class="col-6">
+        <div class="card-body">
+        <h5 >${producto.nombreProducto}</h5>
+        <p c>$${producto.precio}</p>
+        <small >Cantidad = ${producto.cantidad}</small>
+        </p>
+        </div>
+        </div>
+        </div>       
+        `;
+
+    cardsCarrito.append(cardCarrito);
+  });
+  // const botonAgregarAlCarrito = document.querySelectorAll('.agregarAlCarrito')
+  // botonAgregarAlCarrito.forEach((boton) => {
+  //     boton.addEventListener('click', agregarProdusctoCarrito)
+  //})
+}
+
+if (usuarioLogueado != "deslog") {
+  const logoLogin = document.querySelectorAll(".logoLogin");
+  const saludo = document.querySelectorAll(".saludo");
+  saludo[0].innerHTML = `Hola, ${usuarios[usuarioLogueado].nombreUsuario}`;
+  saludo[1].innerHTML = `Hola, ${usuarios[usuarioLogueado].nombreUsuario}`;
+  logoLogin[0].style.backgroundColor = "#66c70c";
+  logoLogin[0].style.borderRadius = "40px";
+  logoLogin[1].style.backgroundColor = "#66c70c";
+  logoLogin[1].style.borderRadius = "40px";
+  if (usuarios[usuarioLogueado].categoria == "admin") {
+    const adminVisibleM = document.querySelector(".adminVisibleM");
+    const adminVisibleD = document.querySelector(".adminVisibleD");
+    adminVisibleD.classList = "nav-link d-block adminVisibleD";
+    adminVisibleM.classList = "nav-item d-block adminVisibleM";
+  }
+}
--- js/tienda.js
+++ js/tienda.js
@@ -1,14 +1,12 @@
-
-const cardProductos = document.getElementById('productosTienda')
-
+const cardProductos = document.getElementById("productosTienda");
 
 const renderizarTienda = () => {
-    productos.forEach((producto) => {
-        const cardProducto = document.createElement('div') 
-        cardProducto.classList = 'card m-auto mb-3 agregarAlCarrito'
-        cardProducto.setAttribute('idProd', producto.id)
-        cardProducto.style = "width: 18rem;"
-        cardProducto.innerHTML = `        
+  productos.forEach((producto) => {
+    const cardProducto = document.createElement("div");
+    cardProducto.classList = "card m-auto mb-3 agregarAlCarrito";
+    cardProducto.setAttribute("idProd", producto.id);
+    cardProducto.style = "width: 18rem;";
+    cardProducto.innerHTML = `        
         <img src="${producto.imagenProducto}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${producto.nombreProducto}</h5>
@@ -16,44 +14,54 @@ const renderizarTienda = () => {
           <p class="card-text">$${producto.precio}</p>
           <button class="btn btn-primary ">Agregar al Carrito</button>
         </div>
-      </div>`     
-        cardProductos.append(cardProducto)
-    })
-    const botonAgregarAlCarrito = document.querySelectorAll('.agregarAlCarrito')
-    botonAgregarAlCarrito.forEach((boton) => {
-        boton.addEventListener('click', agregarProdusctoCarrito)
-    })
-0
-}
+      </div>`;
+    cardProductos.append(cardProducto);
+  });
+  const botonAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito");
+  botonAgregarAlCarrito.forEach((boton) => {
+    boton.addEventListener("click", agregarProdusctoCarrito);
+  });
+};
 
 function agregarProdusctoCarrito(e) {
-    
-    
-    const productoSeleccionado = e.target.closest('.agregarAlCarrito').getAttribute("idProd")
-    console.log(productoSeleccionado);
-    const indexProd = productos.findIndex(
-        (posicion) => posicion.id == productoSeleccionado
-        );
-        console.log(indexProd);  
+  const productoSeleccionado = e.target
+    .closest(".agregarAlCarrito")
+    .getAttribute("idProd");
+  const indexProd = productos.findIndex(
+    (posicion) => posicion.id == productoSeleccionado
+  );
+
+  if (
+    carrito.some(
+      (producto) =>
+        producto.nombreProducto == productos[indexProd].nombreProducto
+    ) == true
+  ) {
+    const indexSumarProd = carrito.findIndex(
+      (posicion) => posicion.id == productoSeleccionado
+    );
+    carrito[indexSumarProd].cantidad = carrito[indexSumarProd].cantidad + 1;
+  } else {
     class ProductoCarrito {
-        constructor(nombreProducto, precio, imagen) {
-          this.nombreProducto = nombreProducto;
-          this.precio = precio;
-          this.imagen = imagen;
-        }
-      }    
-       const prod = new ProductoCarrito(
-        productos[indexProd].nombreProducto,
-        productos[indexProd].precio,
-        productos[indexProd].imagen,
-      );
+      constructor(nombreProducto, precio, imagen, cantidad, id) {
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.cantidad = cantidad;
+        this.id = id;
+      }
+    }
+    const prod = new ProductoCarrito(
+      productos[indexProd].nombreProducto,
+      productos[indexProd].precio,
+      productos[indexProd].imagenProducto,
+      1,
+      productos[indexProd].id
+    );
 
-      carrito.push(prod);
-      localStorage.setItem('carrito', JSON.stringify(carrito))
-      console.log(carrito);
-       
-       
-   
-        }
+    carrito.push(prod);
+  }
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
 
-        renderizarTienda()
+renderizarTienda();
