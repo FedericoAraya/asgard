7c2607daa301cdf3461d4bc4b983efbe840b1def
Federico Araya <federico.araya@gmail.com>
2022-11-17 23:29:53 -0300

    tienda con botones

--- js/tienda.js
+++ js/tienda.js
@@ -1,47 +1,59 @@
-// Variables
 
-// Selectores
-// const searchBarInput = document.querySelector('#searchBarInput')
-// const searchBarButton = document.querySelector('#searchBarButton')
 const cardProductos = document.getElementById('productosTienda')
 
 
-// Funciones
 const renderizarTienda = () => {
     productos.forEach((producto) => {
         const cardProducto = document.createElement('div') 
-        cardProducto.classList = 'card m-auto mb-3'
+        cardProducto.classList = 'card m-auto mb-3 agregarAlCarrito'
+        cardProducto.setAttribute('idProd', producto.id)
         cardProducto.style = "width: 18rem;"
-        cardProducto.innerHTML = `
-        
+        cardProducto.innerHTML = `        
         <img src="${producto.imagenProducto}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${producto.nombreProducto}</h5>
           <p class="card-text">${producto.categoria}</p>
           <p class="card-text">$${producto.precio}</p>
-          <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+          <button class="btn btn-primary ">Agregar al Carrito</button>
         </div>
-      </div>`
-
-      
-
-        console.log(cardProducto);
+      </div>`     
         cardProductos.append(cardProducto)
     })
-   /* const buttonsCTA = document.querySelectorAll('.buttonCTA')
-    buttonsCTA.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            console.log(e.target);
-        })
-    })*/
+    const botonAgregarAlCarrito = document.querySelectorAll('.agregarAlCarrito')
+    botonAgregarAlCarrito.forEach((boton) => {
+        boton.addEventListener('click', agregarProdusctoCarrito)
+    })
+0
 }
 
+function agregarProdusctoCarrito(e) {
+    
+    
+    const productoSeleccionado = e.target.closest('.agregarAlCarrito').getAttribute("idProd")
+    console.log(productoSeleccionado);
+    const indexProd = productos.findIndex(
+        (posicion) => posicion.id == productoSeleccionado
+        );
+        console.log(indexProd);  
+    class ProductoCarrito {
+        constructor(nombreProducto, precio, imagen) {
+          this.nombreProducto = nombreProducto;
+          this.precio = precio;
+          this.imagen = imagen;
+        }
+      }    
+       const prod = new ProductoCarrito(
+        productos[indexProd].nombreProducto,
+        productos[indexProd].precio,
+        productos[indexProd].imagen,
+      );
 
-// Listeners
-/*
-searchBarButton.addEventListener('click', () => {
-    console.log(searchBarInput.value);
-})*/
+      carrito.push(prod);
+      localStorage.setItem('carrito', JSON.stringify(carrito))
+      console.log(carrito);
+       
+       
+   
+        }
 
-// Ejecuciones
-renderizarTienda()
+        renderizarTienda()
